1d2588460f90ae7ee09dc84ce90a5420cf645140
rafaangel85altran <34768304+rafaangel85altran@users.noreply.github.com>
2019-07-08 12:42:37 +0200

    Add state led function

--- Analog_reader_maceta/main+.cpp
+++ Analog_reader_maceta/main+.cpp
@@ -7,8 +7,24 @@ Lee la señal analógica A0 y la mapea para publicarla a 9600 Baudios por puerto s
 
 #include <Arduino.h>
 
+char Mode = '';
+
+void stateLed(char Mode) {
+	if (Mode == 'ComOk') {
+		digitalWrite(LED_BUILTIN, HIGH); 	// COM OK = Led always ON
+	} else if (Mode == 'ComNok') {
+		digitalWrite(LED_BUILTIN, HIGH); 	// COM NOK = Led gives two blinks in 1 sec
+		delay(250);
+		digitalWrite(LED_BUILTIN, LOW);
+		delay(250);
+	} else if (Mode == 'Unknow') {
+		digitalWrite(LED_BUILTIN, LOW);		//COM Unknow = LED always OFF
+	}
+}
+
 void setup() {
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
@@ -16,4 +32,5 @@ void loop() {
   int Humidity = map(sensorValue, 0, 1024, 100, 0);
   Serial.println(Humidity);
   delay(1000);
+  void stateLed();
 }
